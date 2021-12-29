1d88badeb725984eca8e4f7e14de52587a64883c
swethav08711 <shwe08711@gmail.com>
2021-12-29 22:28:02 +0530

    creating template and setting up redux

--- src/container/ProductDetail.js
+++ src/container/ProductDetail.js
@@ -1,6 +1,20 @@
-import React from "react"
+import axios from "axios"
+import React, { useEffect } from "react"
+import { useDispatch } from "react-redux"
 
+import { useParams } from "react-router-dom"
 const ProductDetail = () => {
+  const { productId } = useParams()
+  const dispatch = useDispatch()
+  console.log(productId)
+  const fetchProductDetail = () => {
+    const response = await axios
+      .get(`http://localhost:2022/product/${productId}`)
+      .catch(err => {
+        console.log(err)
+      })
+    dispatch()
+  }
   return <div>deatail</div>
 }
 
