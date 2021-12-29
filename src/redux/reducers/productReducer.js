b07c7996152db02bfce2977ea3ea06934a6abfb0
swethav08711 <shwe08711@gmail.com>
2021-12-29 21:04:16 +0530

    creating template and setting up redux

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -13,7 +13,7 @@ const initialState = {
     },
   ],
 }
-export const productReducer = (state, { type, payload }) => {
+export const productReducer = (state = initialState, { type, payload }) => {
   switch (type) {
     case ActionTypes.SET_PRODUCTS:
       return state
