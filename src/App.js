0d5dfdb7cced249dfc4fe4f4c0e2903bad715b6f
swethav08711 <shwe08711@gmail.com>
2022-01-05 14:02:41 +0530

    changing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Header from "./container/Header"
-import ProductDetail from "./container/ProductDetail"
-import ProductList from "./container/ProductList"
-
 function App() {
-  return (
-    <div>
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={ProductList} />
-          <Route path="/product/:productId" exact component={ProductDetail} />
-          <Route>404 Not Found</Route>
-        </Switch>
-      </Router>
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
